44ed973d8427422b5dbfc7ed0978044f132035c6
Kauan Nogueira Carvalho <Kauan.carvalho.359@gmail.com>
2025-03-15 22:16:12 -0300

    :recycle:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || '';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  // Configurações necessárias para o GitHub Pages
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+  // Necessário para exportação estática
+  output: 'export',
+  // Desabilitar trailing slash
+  trailingSlash: false,
 };
 
 export default nextConfig;
